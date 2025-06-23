aea7422d7da34127176d977db9cf3e365b99466c
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-06-23 13:21:31 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -27,8 +27,8 @@ def create_app():
             # Logging konfigurieren
     app.logger.setLevel(logging.INFO)
 
-    # CORS für alle Routen aktivieren und die Frontend-URL erlauben
-    CORS(app, resources={r"/*": {"origins": "https://masterthesis-1.onrender.com"}})
+    # CORS für alle Routen und alle Ursprünge aktivieren
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     # Blueprints registrieren, um die Routen zu aktivieren
     app.register_blueprint(pages_bp)
