e2bb91590978cd002dd9c9fd8d2d93806a77bdeb
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-06-24 17:39:01 +0200

    Leerzeichen zu viel

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,6 @@
 // export const API_BASE_URL = "http://localhost:5100";
-export const API_BASE_URL = "https://omissions-alot-match-dod.trycloudflare.com ";
+
+// cloudflared tunnel --url http://localhost:5100
+// copy url and push on github
+
+export const API_BASE_URL = "https://omissions-alot-match-dod.trycloudflare.com";
