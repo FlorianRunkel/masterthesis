9b7aef82f99130209c94f9dc1b2f54d312ab82c3
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-06-24 17:35:04 +0200

    api change cloudflare

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:5100";
-//export const API_BASE_URL = "https://1409-93-104-240-206.ngrok-free.app";
+// export const API_BASE_URL = "http://localhost:5100";
+export const API_BASE_URL = "https://omissions-alot-match-dod.trycloudflare.com ";
