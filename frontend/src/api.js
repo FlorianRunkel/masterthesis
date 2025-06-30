ee113b2b5d80963b1ae732df6f5019a04f8a21ec
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-06-30 12:35:04 +0200

    go live

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,4 +3,4 @@
 // cloudflared tunnel --url http://localhost:5100
 // copy url and push on github
 
-export const API_BASE_URL = "https://chicks-mills-geometry-houston.trycloudflare.com";
+export const API_BASE_URL = "https://edge-conventional-concord-domain.trycloudflare.com";
