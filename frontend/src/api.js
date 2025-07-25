b169cab5880c310099628df2d8c77c88bc3f717a
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-07-25 08:40:40 +0200

    updated api key

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = "http://localhost:5100";
 
 // cloudflared tunnel --url http://localhost:5100 copy url and push on github
-export const API_BASE_URL = "https://elliott-stuck-wax-catering.trycloudflare.com";
+export const API_BASE_URL = "https://separation-mixture-hereby-bench.trycloudflare.com";
