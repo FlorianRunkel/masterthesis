649bb20f8268ac2bd6d03967f3595071f9144d47
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-07-29 15:51:26 +0200

    update api key

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = "http://localhost:5100";
 
 //cloudflared tunnel --url http://localhost:5100 copy url and push on github
-export const API_BASE_URL = "https://unnecessary-exclude-terrace-specialists.trycloudflare.com";
+export const API_BASE_URL = "https://waters-cpu-gently-shuttle.trycloudflare.com";
