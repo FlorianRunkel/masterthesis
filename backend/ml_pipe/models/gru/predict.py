2fdd5cf0fd4b4a5e185ec9f8aae2a130bda9f327
FloRunkel <123296126+FloRunkel@users.noreply.github.com>
2025-08-01 14:18:09 +0200

    update api

--- backend/ml_pipe/models/gru/predict.py
+++ backend/ml_pipe/models/gru/predict.py
@@ -36,7 +36,7 @@ def get_feature_names():
         "job changes total", 
         "job positions total",
         "job average duration",
-        #"education highest degree",
+        "education highest degree",
         "age category",
         "position level",
         "industry",
@@ -251,7 +251,7 @@ def prepare_features(profile_dict):
             float(latest_sample.get("anzahl_wechsel_bisher", 0) or 0),
             float(latest_sample.get("anzahl_jobs_bisher", 0) or 0),
             float(latest_sample.get("durchschnittsdauer_bisheriger_jobs", 0) or 0),
-            #float(extract_highest_degree(education_data) or 0),
+            float(extract_highest_degree(education_data) or 0),
             float(estimate_age_category(profile_info) or 0),
             #float(extract_anzahl_standortwechsel(experiences) or 0),
             #float(fe.get_study_field_num(extract_study_field(education_data)) or 0)
@@ -338,7 +338,7 @@ Load model
 '''
 def load_model(model_path):
     checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
-    model = GRUModel(seq_input_size=15, hidden_size=128, num_layers=4, dropout=0.2, lr=0.0003)
+    model = GRUModel(seq_input_size=16, hidden_size=128, num_layers=4, dropout=0.2, lr=0.0003)
     model.load_state_dict(checkpoint)
     model.eval()
 
@@ -353,7 +353,7 @@ def load_model(model_path):
 Create background data
 '''
 def create_background_data(seq_tensor):
-    background_seq = torch.zeros((10, seq_tensor.shape[1], 15))  # 12 Features
+    background_seq = torch.zeros((10, seq_tensor.shape[1], 16))  # 12 Features
     return background_seq
 
 '''
